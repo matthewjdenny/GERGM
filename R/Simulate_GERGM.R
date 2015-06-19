e1dec0194c314b6b6b06e8e7db3a75973312f6f7
Matt Denny <matthewjdenny@gmail.com>
2015-06-19 10:29:47 -0400

    removed comments and default values

--- R/Simulate_GERGM.R
+++ R/Simulate_GERGM.R
@@ -1,12 +1,12 @@
 # Simulate a gergm
 Simulate_GERGM <- function(object, nsim, seed = NULL,
-                           method = c("Gibbs", "Metropolis"), MCMC.burnin = 100,  
+                           method , MCMC.burnin ,  
                            weights = object@weights,
                            coef = object@theta.coef, formula = object@formula,
-                           thin = 1, shape.parameter = 1, together = 0,
-                           seed1 = 10000) {
+                           thin , shape.parameter , together ,
+                           seed1,
+						   possible.stats ) {
   # object: an object of class "gergm"
-  method <- method[1]
   
   theta <- as.numeric(coef[1,])  # obtain the estimated thetas
   
@@ -18,7 +18,7 @@ Simulate_GERGM <- function(object, nsim, seed = NULL,
   rhs <- paste(rhs, collapse = "+")
   formula.new <- as.formula(paste0("net ~ ", rhs))
   
-  res1 <- weighted.ergm.data(formula.new, theta = theta[abs(theta) > 0], 
+  res1 <- Parse_Formula_Object(formula.new, possible.stats, theta = theta[abs(theta) > 0], 
                              alpha = alpha[abs(theta) > 0])
   statistics <- res1$statistics
   
@@ -51,26 +51,9 @@ Simulate_GERGM <- function(object, nsim, seed = NULL,
     #fix alphas for MH sims
     alphas[which(is.na(alphas))] = 1
     alphas[which(alphas == 0)] = 1
-    #print to check what parameters are going in to the MH sampler
-    #print(Sys.time())
-    #print("Statistics to use")
-    #print(statistics)
+
     print("Thetas")
     print(thetas)
-    #print("Alphas")
-    #print(alphas)
-    #print("Take sample every:")
-    #print(sample_every)
-    #print("Shape parameter:")
-    #print(shape.parameter)
-    #print("Burnin")
-    #print(MCMC.burnin)
-    #print("Number of Nodes")
-    #print(num.nodes)
-    #print("together")
-    #print(together)
-    #print("seed")
-    #print(seed1)
     
     samples <- MH(
       number_of_iterations = nsim + MCMC.burnin, 
@@ -93,34 +76,6 @@ Simulate_GERGM <- function(object, nsim, seed = NULL,
     acceptance.rate <- mean(samples[[1]])
     cat("acceptance.rate", acceptance.rate, "\n")
     
-    # hack to make sure we do not have severe autocorrelation in our sample
-    #densities <- h.statistics[,1]
-    #cat("number of raw samples:",length(densities),"\n")
-    #ar1 <- cor(densities[2:length(densities)],densities[1:(length(densities)-1)])
-    #print(ar1)
-    
-    #         thin <- 1
-    #         while(abs(ar1) > .3){
-    #             thin = thin +1
-    #             thinSeq <- round(seq(1,length(densities),by=thin))
-    #             thinDens <- densities[thinSeq]
-    #             ar1 <- cor(thinDens[2:length(thinDens)],thinDens[1:(length(thinDens)-1)])
-    #             #print(paste(ar1,thin))
-    #         }
-    #         print(thin)
-    #         print(ar1)
-    #thin = 200
-    
-    #thinSeq <- round(seq(1,length(h.statistics[,1]),by=thin))
-    
-    #h.statistics <- h.statistics[thinSeq,]
-    
-    
-    #h.statistics <- t(apply(nets, 3, h2, triples = triples,
-    #                        statistics = rep(1, 6), together = together))
-    #hack to add measurement error
-    #h.statistics <- h.statistics + runif(6*nsim, min = -100, max = 100)
-    
   }
   h.statistics = data.frame(out2stars = h.statistics[, 1], 
                             in2stars = h.statistics[, 2], 
@@ -128,10 +83,7 @@ Simulate_GERGM <- function(object, nsim, seed = NULL,
                             recip = h.statistics[, 4], 
                             ttriads = h.statistics[, 5], 
                             edgeweight = h.statistics[, 6])
-  #possible.stats <- c("out2stars", "in2stars", "ctriads", "recip",
-  #  "ttriads", "edgeweight")
-  #colnames(h.statistics) <- possible.stats
-  #h.statistics <- as.data.frame(h.statistics)
+
   return(list(Networks = nets, Statistics = h.statistics,
               Acceptance.rate = acceptance.rate))
 }
