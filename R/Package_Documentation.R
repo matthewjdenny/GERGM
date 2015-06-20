c82471204de2579d8d1d6e025c75e9e7424dac7e
Matt Denny <matthewjdenny@gmail.com>
2015-06-20 16:32:09 -0400

    package documentation and library methods import so we can define GERGM class from command line

---
+++ R/Package_Documentation.R
@@ -0,0 +1,13 @@
+#' GERGM: A package for estimating and diagnosing Generalized Exponential Random
+#' Graph Models
+#'
+#' @section GERGM functions:
+#' There will be a few
+#'
+#' @docType package
+#' @name GERGM
+NULL
+#> NULL
+
+#' @import methods
+NULL
