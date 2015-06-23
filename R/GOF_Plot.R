9144b6d9297d8ffc29b698529f00d06e7a7be6f3
Matt Denny <matthewjdenny@gmail.com>
2015-06-23 09:04:48 -0400

    reworked to GOF plot function to plot standardized, normalized boxplots

--- R/GOF_Plot.R
+++ R/GOF_Plot.R
@@ -1,10 +1,19 @@
 # Function for plotting goodness of fit statistics in a boxplot
 Gof_Plot <- function(GERGM_Object){
-  # simulation.obj is a list object resulting from simulations
-  # gergm.obj is an object of class "gergm"
-  boxplot(GERGM_Object@MCMC_output$Statistics, medcol = "red")
-  boxplot(rbind(GERGM_Object@stats[2, ], GERGM_Object@stats[2, ]), add =T,
-          medcol="blue", names = F)
+  #define colors
+  UMASS_BLUE <- rgb(51,51,153,255,maxColorValue = 255)
+  UMASS_RED <- rgb(153,0,51,255,maxColorValue = 255)
+
+  #input is the GERGM_Object, we are going to normalize all statistics
+  temp <- (GERGM_Object@MCMC_output$Statistics - GERGM_Object@stats[2, ])/apply(GERGM_Object@MCMC_output$Statistics,1,sd)
+
+  boxplot(temp, medcol = UMASS_RED,
+          xlab = "Network Statistic",
+          ylab = "Standardized, Normalized Values",
+          main = "Blue = Observed Statistic, Red = Simulated Mean")
+  zero_line <- rep(0,length(GERGM_Object@stats[2, ]))
+  zero_plot <- rbind(zero_line,zero_line)
+  boxplot(zero_plot, add =T, medcol=UMASS_BLUE, names = F)
 }
 
 Comparison_GOF_Plot <- function(MH.obj, Gibbs.obj, gergm.obj){
