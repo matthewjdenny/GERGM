ef95538e7ec355ecd79ac7a4ab81ddcbb623a5e9
Matt Denny <matthewjdenny@gmail.com>
2015-06-24 16:38:22 -0400

    Added trace plot that is generated automatically

--- R/GOF_Plot.R
+++ R/GOF_Plot.R
@@ -48,6 +48,21 @@ Estimate_Plot <- function(GERGM_Object){
   print(zp1)
 }
 
+Trace_Plot <- function(GERGM_Object){
+  UMASS_BLUE <- rgb(51,51,153,255,maxColorValue = 255)
+  UMASS_RED <- rgb(153,0,51,255,maxColorValue = 255)
+  stats <- GERGM_Object@MCMC_output$Statistics
+  indexes <- 1:length(stats[,1])
+  stats <- cbind(stats,indexes)
+  p <- ggplot2::ggplot(stats, aes(x=indexes, y=edgeweight ))
+  p  <- p + ggplot2::geom_line(color = UMASS_BLUE) +
+    ggplot2::xlab("Iteration") +
+    ggplot2::ylab("Network Density") +
+    ggplot2::ggtitle("Trace Plot of Density for Simulated Networks")
+  print(p)
+}
+
+
 Comparison_GOF_Plot <- function(MH.obj, Gibbs.obj, gergm.obj){
   par(mfrow = c(2,3))
   violins(data.frame(Gibbs.obj$Statistics[,1], MH.obj$Statistics[,1]), names = c("Gibbs", "M-H"), col = c("blue", "green"), connectcol = "transparent", main = "Out-2-Stars")
