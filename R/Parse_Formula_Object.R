28ff90d2fa6ae242d0c1720fad400200a61a07b6
Matt Denny <matthewjdenny@gmail.com>
2015-08-07 00:17:41 -0400

    functionality to parse different classes to terms separately

--- R/Parse_Formula_Object.R
+++ R/Parse_Formula_Object.R
@@ -1,18 +1,25 @@
-#' convert a formula object to statistics and network of interest
-#' (formerly weighted.ergm.data)
 Parse_Formula_Object <- function(formula,
-                                 possible.stats,
+                                 possible_structural_terms,
+                                 possible_covariate_terms,
+                                 possible_network_terms,
+                                 raw_network = NULL,
                                  theta = NULL,
-                                 alpha = NULL) {
+                                 terms_to_parse = "structural") {
 
-  num_stats <- length(possible.stats)
+  num_stats <- length(possible_structural_terms)
 
   # parse the formula
   if (class(formula) != "formula") {
     stop("'formula' must be a formula object.")
   }
   lhs <- deparse(formula[[2]])  # name of the response variable
-  net <- eval(parse(text = lhs))  # get the actual response data
+
+  # get the actual response data
+  if(is.null(raw_network)){
+    net <- as.matrix(get(lhs))
+  }else{
+    net <- raw_network
+  }
 
   # check that the response is a matrix
   if (class(net) != "matrix") {
@@ -20,44 +27,91 @@ Parse_Formula_Object <- function(formula,
   }
 
   rhs <- paste0(deparse(formula[[3]]), collapse = "")  # rhs of formula
-  rhs <- gsub("\\s+", " ", rhs)  # get rid of redundant spaces
-  rhs <- strsplit(rhs, " \\+ ")[[1]]  # parse separate formula elements
+  rhs <- gsub("\\s+", "", rhs)  # get rid of redundant spaces
+  rhs <- strsplit(rhs, "\\+")[[1]]  # parse separate formula elements
+  parsed_rhs <- vector (length = length(rhs), mode = "list")
+  rhs_term_names <- rep("", length(rhs))
+  alpha <- rep(1, length(rhs))
+  for (i in 1:length(rhs)){
+    parsed_rhs[[i]] <- parse_formula_term(rhs[i],
+                                         possible_structural_terms,
+                                         possible_covariate_terms,
+                                         possible_network_terms)
+    rhs_term_names[i] <- parsed_rhs[[i]]$term
+    alpha[i] <- parsed_rhs[[i]]$weight
+  }
+  # if we are parsing the structural terms out of the formula
+  if(terms_to_parse == "structural"){
+    # remove all node level covariate terms
+    remove <- which(rhs_term_names %in% possible_covariate_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    remove <- which(rhs_term_names %in% possible_network_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    # check that the names of the statistics match those that are possible
+    possible <- 1:length(rhs_term_names)
+    actual <- which(rhs_term_names %in% possible_structural_terms)
+    if (length(possible) != length(actual)) {
+      stop(paste("the specified variable", possible_structural_terms[setdiff(possible, actual)],
+                 "is not an available statistic.", sep = " "))
+    }
 
-  # check that the names of the statistics match those that are possible
-  possible <- 1:length(rhs)
-  actual <- which(rhs %in% possible.stats)
-  if (length(possible) != length(actual)) {
-    stop(paste("the specified variable", rhs[setdiff(possible, actual)],
-               "is not an available statistic.", sep = " "))
-  }
+    # if theta is NULL, assume all ones
+    if (is.null(theta) == TRUE) {
+      theta <- rep(1, length(rhs_term_names))
+    }
 
-  # if alpha is NULL, assume all ones
-  if (is.null(alpha) == TRUE) {
-    alpha <- rep(1, length(rhs))
+    # check that theta and the number of statistics are equal
+    if (length(rhs_term_names) != length(theta)) {
+      stop("'theta' must be the same length as the number of statistics")
+    }
+    stat.indx <- which(possible_structural_terms %in% rhs_term_names)
+    statistics <- rep(0, num_stats)
+    statistics[stat.indx] <- 1
+    alphas <- rep(1, num_stats)
+    thetas <- rep(0, num_stats)
+    for (i in 1:length(rhs_term_names)) {
+      alphas[which(rhs_term_names[i] == possible_structural_terms)] <- alpha[i]
+      thetas[which(rhs_term_names[i] == possible_structural_terms)] <- theta[i]
+    }
+    return(list(net = net,
+                statistics = statistics,
+                alphas = alphas,
+                thetas = thetas))
   }
-
-  # if theta is NULL, assume all ones
-  if (is.null(theta) == TRUE) {
-    theta <- rep(1, length(rhs))
+  if(terms_to_parse == "covariate"){
+    # if we are parsing covariate terms out of the formula
+    remove <- which(rhs_term_names %in% possible_structural_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    remove <- which(rhs_term_names %in% possible_network_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    parsed_rhs <- append(parsed_rhs,list(network = net))
+    return(parsed_rhs)
   }
-  # check that alpha and the number of statistics are equal
-  if (length(rhs) != length(alpha)) {
-    stop("'alpha' must be the same length as the number of statistics")
+  if(terms_to_parse == "network"){
+    # if we are parsing network terms out of the formula
+    remove <- which(rhs_term_names %in% possible_structural_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    remove <- which(rhs_term_names %in% possible_covariate_terms)
+    if (length(remove) > 0){
+      rhs_term_names <- rhs_term_names[-remove]
+      parsed_rhs <- parsed_rhs[-remove]
+    }
+    parsed_rhs <- append(parsed_rhs,list(network = net))
+    return(parsed_rhs)
   }
-
-  # check that theta and the number of statistics are equal
-  if (length(rhs) != length(theta)) {
-    stop("'theta' must be the same length as the number of statistics")
-  }
-  stat.indx <- which(possible.stats %in% rhs)
-  statistics <- rep(0, num_stats)
-  statistics[stat.indx] <- 1
-  alphas <- rep(1, num_stats)
-  thetas <- rep(0, num_stats)
-  for (i in 1:length(rhs)) {
-    alphas[which(rhs[i] == possible.stats)] <- alpha[i]
-    thetas[which(rhs[i] == possible.stats)] <- theta[i]
-  }
-  return(list(net = net, statistics = statistics,
-              alphas = alphas, thetas = thetas))
 }
