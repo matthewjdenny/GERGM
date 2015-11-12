b341f96f254b5beb368c862216749f7320dea707
Matt Denny <matthewjdenny@gmail.com>
2015-11-12 12:09:37 -0500

    added option to box plot raw statistics

--- R/GOF.R
+++ R/GOF.R
@@ -8,14 +8,23 @@ GOF <- function(GERGM_Object){
   UMASS_BLUE <- rgb(51,51,153,255,maxColorValue = 255)
   UMASS_RED <- rgb(153,0,51,255,maxColorValue = 255)
 
-  #input is the GERGM_Object, we are going to normalize all statistics
-  temp <- (GERGM_Object@MCMC_output$Statistics - GERGM_Object@stats[2, ])/apply(GERGM_Object@MCMC_output$Statistics,1,sd)
+  if(GERGM_Object@simulation_only){
+    temp <- GERGM_Object@MCMC_output$Statistics
 
-  boxplot(temp, medcol = UMASS_RED,
-          xlab = "Network Statistic",
-          ylab = "Standardized, Normalized Values",
-          main = "Blue = Observed Statistic, Red = Simulated Mean")
-  zero_line <- rep(0,length(GERGM_Object@stats[2, ]))
-  zero_plot <- rbind(zero_line,zero_line)
-  boxplot(zero_plot, add =T, medcol=UMASS_BLUE, names = F)
+    boxplot(temp, medcol = UMASS_BLUE,
+            xlab = "Network Statistic",
+            ylab = "Statistic Values",
+            main = "Simulated Network Statistics")
+  }else{
+    #input is the GERGM_Object, we are going to normalize all statistics
+    temp <- (GERGM_Object@MCMC_output$Statistics - GERGM_Object@stats[2, ])/apply(GERGM_Object@MCMC_output$Statistics,1,sd)
+
+    boxplot(temp, medcol = UMASS_RED,
+            xlab = "Network Statistic",
+            ylab = "Standardized, Normalized Values",
+            main = "Blue = Observed Statistic, Red = Simulated Mean")
+    zero_line <- rep(0,length(GERGM_Object@stats[2, ]))
+    zero_plot <- rbind(zero_line,zero_line)
+    boxplot(zero_plot, add =T, medcol=UMASS_BLUE, names = F)
+  }
 }
