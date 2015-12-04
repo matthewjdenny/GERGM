dd67a5385fa15c92af7c21e48f78f48454df42c4
Matt Denny <matthewjdenny@gmail.com>
2015-12-03 20:29:51 -0500

    turns out we cannot run out tests using testthat, will have to do so manually

--- tests/testthat/test_gergm.R
+++ tests/testthat/test_gergm.R
@@ -2,9 +2,36 @@ test_that("Simple model with no covariates runs", {
   skip_on_cran()
   ########################### 1. No Covariates #############################
   # Preparing an unbounded network without covariates for gergm estimation #
+  skip("Skipping test as it can only be run in the global environment.")
+
   set.seed(12345)
   net <- matrix(rnorm(100,0,20),10,10)
   colnames(net) <- rownames(net) <- letters[1:10]
+
+  # one parameter model
+  formula <- net ~ ttriads
+
+  test <- gergm(formula,
+                normalization_type = "division",
+                network_is_directed = TRUE,
+                use_MPLE_only = FALSE,
+                estimation_method = "Metropolis",
+                maximum_number_of_lambda_updates = 1,
+                maximum_number_of_theta_updates = 5,
+                number_of_networks_to_simulate = 40000,
+                thin = 1/10,
+                proposal_variance = 0.5,
+                downweight_statistics_together = TRUE,
+                MCMC_burnin = 10000,
+                seed = 456,
+                convergence_tolerance = 0.01,
+                MPLE_gain_factor = 0,
+                force_x_theta_updates = 4)
+
+  check_against <- c(0.067)
+  expect_equal(round(as.numeric(test@theta.coef[1,]),3), check_against)
+
+  # three parameter model
   formula <- net ~ recip + edges + ttriads
 
   test <- gergm(formula,
@@ -24,9 +51,13 @@ test_that("Simple model with no covariates runs", {
                 MPLE_gain_factor = 0,
                 force_x_theta_updates = 4)
 
-formula2 <- net ~ recip + edges + ttriads + in2star + ctriads
+  check_against <- c(1.872, -0.005, -0.484)
+  expect_equal(round(as.numeric(test@theta.coef[1,]),3), check_against)
 
-test <- gergm(formula2,
+  # five parameter model
+  formula2 <- net ~ recip + edges + ttriads + in2star + ctriads
+
+  test <- gergm(formula2,
               normalization_type = "division",
               network_is_directed = TRUE,
               use_MPLE_only = FALSE,
@@ -42,10 +73,18 @@ test <- gergm(formula2,
               convergence_tolerance = 0.01,
               MPLE_gain_factor = 0,
               force_x_theta_updates = 4)
+
+check_against <- c(-0.039,  1.147,  2.084, -0.302, -1.043)
+expect_equal(round(as.numeric(test@theta.coef[1,]),3), check_against)
+
 })
 
 test_that("Model with covariates runs", {
   skip_on_cran()
+  print(environment())
+
+  skip("Skipping test as it can only be run in the global environment.")
+
   set.seed(12345)
   net <- matrix(runif(100,0,1),10,10)
   colnames(net) <- rownames(net) <- letters[1:10]
@@ -73,4 +112,9 @@ test_that("Model with covariates runs", {
                 convergence_tolerance = 0.01,
                 MPLE_gain_factor = 0,
                 force_x_theta_updates = 2)
+
+  check_against <- c(2.037, -0.070, -0.582, 0.582, -0.227, -0.043, -0.040,  0.105, -1.877)
+  check <- c(round(as.numeric(test@theta.coef[1,]),3),round(as.numeric(test@lambda.coef[1,]),3))
+  expect_equal(check, check_against)
+
 })
