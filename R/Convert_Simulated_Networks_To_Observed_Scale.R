f09e23afbb9a7abb152f1c95c31be1c05fb31a6d
Matt Denny <matthewjdenny@gmail.com>
2015-12-09 11:05:31 -0500

    outline of conversion function

---
+++ R/Convert_Simulated_Networks_To_Observed_Scale.R
@@ -0,0 +1,9 @@
+Convert_Simulated_Networks_To_Observed_Scale <- function(GERGM_Object,
+                                        normalization_type = c("log","division"),
+                                        transformation_type){
+  # figure out what kind of transformation we did, then convert back.
+
+
+
+  return(GERGM_Object)
+}
