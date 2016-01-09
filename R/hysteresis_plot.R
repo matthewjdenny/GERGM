4dc2d58335d632c846d420d91b0021835512227a
Matt Denny <matthewjdenny@gmail.com>
2016-01-09 10:21:34 -0500

    get legend, lines and titles right

--- R/hysteresis_plot.R
+++ R/hysteresis_plot.R
@@ -10,7 +10,21 @@ hysteresis_plot <- function(hysteresis_output,
   UMASS_RED <- rgb(153,0,51,255,maxColorValue = 255)
   num_thetas <- length(hysteresis_output[[1]]$theta_values)
   middle <- floor(num_thetas/2) + 1
-  Theta = SE = Density = color = NULL
+  Theta = SE = Density = Order = NULL
+  possible_structural_terms <- c("out2stars",
+                                 "in2stars",
+                                 "ctriads",
+                                 "mutual",
+                                 "ttriads",
+                                 "edges")
+
+
+  pretty_structural_terms <- c("Out Two-Stars",
+                                 "In Two-Stars",
+                                 "Cyclic Triads",
+                                 "Mutual Dyads",
+                                 "Transitive Triads",
+                                 "Network Density")
 
   #loop over thetas
   for(i in 1:length(hysteresis_output)){
@@ -19,34 +33,47 @@ hysteresis_plot <- function(hysteresis_output,
                              Density = hysteresis_output[[i]]$mean_densities,
                              SE = apply(hysteresis_output[[i]]$network_densities,
                                         2,sd),
-                             color = c(rep("Ascending",num_thetas),
+                             Order = c(rep("Ascending",num_thetas),
                                        rep("Descending",num_thetas))
     )
+    index <- which(possible_structural_terms == hysteresis_output[[i]]$term)
+
     data <- data.frame(modelFrame)
 
-    zp1 <- ggplot2::ggplot(data, ggplot2::aes(colour = color)) +
+    zp1 <- ggplot2::ggplot(data, ggplot2::aes(colour = Order, group = Order)) +
       ggplot2::scale_color_manual(values = c(UMASS_BLUE,UMASS_RED))
 
     zp1 <- zp1 + ggplot2::geom_hline(yintercept = 0, colour = gray(1/2), lty = 2)
     zp1 <- zp1 + ggplot2::geom_linerange(
       ggplot2::aes(x = Theta,
+                   y = Density,
                    ymin = Density - SE*(-qnorm((1-0.9)/2)),
                    ymax = Density + SE*(-qnorm((1-0.9)/2))),
-                   lwd = 2,
-                   position = ggplot2::position_dodge(width = 1/2))
-    zp1 <- zp1 + ggplot2::geom_pointrange(
+                   lwd = 1)
+                   #position = ggplot2::position_dodge(width = 1/2))
+    zp1 <- zp1 + ggplot2::geom_linerange(
       ggplot2::aes(x = Theta,
                    y = Density,
                    ymin = Density - SE*(-qnorm((1-0.95)/2)),
                    ymax = Density + SE*(-qnorm((1-0.95)/2))),
-                   lwd = 1,
-                   position = ggplot2::position_dodge(width = 1/2),
-                   shape = 21, fill = "WHITE")
+                   lwd = 0.5) +
+      ggplot2::geom_point(ggplot2::aes(x = Theta,
+                                       y = Density,
+                                       shape= Order,
+                                       colour = Order),   # Shape depends on cond
+                 size = 3)+
+      ggplot2::scale_shape_manual(values=c( 16, 17))
+
 
     zp1 <- zp1  + ggplot2::theme_bw() +
-        ggplot2::theme(legend.position="top") +
-        ggplot2::geom_hline(yintercept = hysteresis_output[[1]]$observed_density) +
-        ggplot2::geom_vline(xintercept = hysteresis_output[[i]]$theta_values[middle])
+      ggplot2::theme(legend.justification = c(1, 0),
+                     legend.position = c(1, 0),
+                     legend.key = ggplot2::element_blank(),
+                     legend.background = ggplot2::element_rect(fill="transparent")) +
+      ggplot2::geom_hline(yintercept = hysteresis_output[[1]]$observed_density) +
+      ggplot2::geom_vline(xintercept = hysteresis_output[[i]]$theta_values[middle])+
+      ggplot2::ggtitle(pretty_structural_terms[index])
+
 
 
     print(zp1)
