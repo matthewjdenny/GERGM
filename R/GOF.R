5172b0bb9492bdd29e83de2db648fa9f584f98f2
Matt Denny <matthewjdenny@gmail.com>
2016-01-17 00:32:00 -0500

    changed object fields and undirected to directed field name + added fields

--- R/GOF.R
+++ R/GOF.R
@@ -11,20 +11,20 @@ GOF <- function(GERGM_Object,
   UMASS_BLUE <- rgb(51,51,153,155,maxColorValue = 255)
   UMASS_RED <- rgb(153,0,51,255,maxColorValue = 255)
 
-  if(GERGM_Object@simulation_only){
+  if (GERGM_Object@simulation_only) {
     temp <- GERGM_Object@MCMC_output$Statistics
 
     boxplot(temp, medcol = UMASS_BLUE,
             xlab = "Network Statistic",
             ylab = "Statistic Values",
             main = "Simulated Network Statistics")
-  }else{
-    if(GERGM_Object@undirected_network){
+  } else {
+    if (!GERGM_Object@directed_network) {
       #input is the GERGM_Object, we are going to normalize all statistics
       temp <- GERGM_Object@MCMC_output$Statistics
 
       temp2 <- apply(GERGM_Object@MCMC_output$Statistics,2,sd)
-      for(i in 1:ncol(temp)){
+      for (i in 1:ncol(temp)) {
         temp[,i] <- temp[,i] - GERGM_Object@stats[2,i]
         temp[,i] <- temp[,i]/temp2[i]
       }
@@ -41,7 +41,7 @@ GOF <- function(GERGM_Object,
       zero_line <- zero_line[c(2,5,6)]
       zero_plot <- rbind(zero_line,zero_line)
       boxplot(zero_plot, add = T, medcol = UMASS_BLUE, names = F)
-    }else{
+    } else {
       #input is the GERGM_Object, we are going to normalize all statistics
       temp <- GERGM_Object@MCMC_output$Statistics
 
