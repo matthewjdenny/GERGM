818ac41183dd319bbf7178a306b4cad8fc224a05
Matt Denny <matthewjdenny@gmail.com>
2016-02-01 16:20:44 -0500

    add try statement to deal with really funky standard errors

--- R/calculate_standard_errors.R
+++ R/calculate_standard_errors.R
@@ -1,14 +1,22 @@
 calculate_standard_errors <- function(hessian,
                                       GERGM_Object){
-  std_errors <- sqrt(diag(solve(-hessian)))
 
-  if(is.null(GERGM_Object@hessians)) {
+  # calculate standard errors inside of try statement to better handle cases
+  # where we end up with degeneracy
+  std_errors <- try(sqrt(diag(solve(-hessian))))
+
+  # if we get an error, set all standard errors to NaN.
+  if (class(std_errors) == "try-error") {
+    std_errors <- matrix(NaN,nrow = nrow(hessian), ncol = nrow(hessian))
+  }
+
+  if (is.null(GERGM_Object@hessians)) {
     GERGM_Object@hessians <- list(hessian)
   } else {
     GERGM_Object@hessians <- append(GERGM_Object@hessians,hessian)
   }
 
-  if(is.null(GERGM_Object@standard_errors)) {
+  if (is.null(GERGM_Object@standard_errors)) {
     GERGM_Object@standard_errors <- list(std_errors)
   } else {
     GERGM_Object@standard_errors <- append(GERGM_Object@standard_errors,
