c3eb1c68de64329bc949a57ca01756c659fa5987
Matt Denny <matthewjdenny@gmail.com>
2016-02-15 20:26:15 -0500

    make sure trace plot always works and scale is comma not scientific

--- R/Trace_Plot.R
+++ R/Trace_Plot.R
@@ -7,19 +7,24 @@
 Trace_Plot <- function(GERGM_Object){
   UMASS_BLUE <- rgb(51,51,153,255,maxColorValue = 255)
   edges <- NULL
-  stats <- GERGM_Object@MCMC_output$Statistics
-  indexes <- 1:length(stats[,1])
-  nr <- nrow(GERGM_Object@network)
-  normalizer <- nr*(nr-1)
-  stats <- cbind(stats/normalizer,indexes)
+  stats <- GERGM_Object@simulated_statistics_for_GOF
 
+  # get the total number of indices
+  indexes <- 1:length(stats[,1])
+
+  iteration_number <- seq(1,GERGM_Object@number_of_simulations,
+                          length.out = length(indexes))
+  nr <- nrow(GERGM_Object@network)
+  normalizer <- nr * (nr - 1)
+  stats <- cbind(stats/normalizer,iteration_number)
   actual_density <- GERGM_Object@stats[2,6]/normalizer
 
-  p <- ggplot2::ggplot(stats, ggplot2::aes(x = indexes, y = edges))
+  p <- ggplot2::ggplot(stats, ggplot2::aes(x = iteration_number, y = edges))
   p  <- p + ggplot2::geom_line(color = UMASS_BLUE) +
     ggplot2::xlab("Iteration") +
-    ggplot2::ylab("Network Density") +
+    ggplot2::ylab("Constrained Network Density") +
     ggplot2::ggtitle("Trace Plot of Density for Simulated Networks") +
-    ggplot2::geom_abline(intercept = actual_density, slope = 0)
+    ggplot2::geom_abline(intercept = actual_density, slope = 0) +
+    ggplot2::scale_x_continuous(labels = scales::comma)
   print(p)
 }
