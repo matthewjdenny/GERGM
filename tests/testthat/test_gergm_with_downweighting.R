e5c0b62c7bcc3c8dcb6ce47958e6e5ac36f6ee4b
Matt Denny <matthewjdenny@gmail.com>
2016-09-18 12:35:09 -0400

    adjust test for fixed theta updates

--- tests/testthat/test_gergm_with_downweighting.R
+++ tests/testthat/test_gergm_with_downweighting.R
@@ -137,7 +137,7 @@ test_that("That weighted MPLE works", {
                 force_x_theta_updates = 1,
                 weighted_MPLE = TRUE)
 
-  check_against <- c(4.100, -0.732)
+  check_against <- c(4.122, -0.731)
   expect_equal(round(as.numeric(test@theta.coef[1,]),3), check_against)
 
 
