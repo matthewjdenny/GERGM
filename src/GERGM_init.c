dc3175c84eb434fda8a9f67ffbd1cfb44d1094b5
Matt Denny <matthewjdenny@gmail.com>
2017-03-23 15:02:02 -0400

    add additional arguments and update from R 3.4

--- src/GERGM_init.c
+++ src/GERGM_init.c
@@ -8,7 +8,8 @@
 */
 
 /* .Call calls */
-extern SEXP GERGM_Extended_Metropolis_Hastings_Sampler(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP GERGM_Distribution_Metropolis_Hastings_Sampler(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP GERGM_Extended_Metropolis_Hastings_Sampler(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP GERGM_extended_weighted_mple_objective(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP GERGM_frobenius_norm(SEXP, SEXP);
 extern SEXP GERGM_get_indiviual_triad_values(SEXP, SEXP, SEXP, SEXP);
@@ -19,15 +20,16 @@ extern SEXP GERGM_Metropolis_Hastings_Sampler(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP
 extern SEXP GERGM_weighted_mple_objective(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"GERGM_Extended_Metropolis_Hastings_Sampler",   (DL_FUNC) &GERGM_Extended_Metropolis_Hastings_Sampler,   28},
-    {"GERGM_extended_weighted_mple_objective",       (DL_FUNC) &GERGM_extended_weighted_mple_objective,       16},
-    {"GERGM_frobenius_norm",                         (DL_FUNC) &GERGM_frobenius_norm,                          2},
-    {"GERGM_get_indiviual_triad_values",             (DL_FUNC) &GERGM_get_indiviual_triad_values,              4},
-    {"GERGM_get_triad_weights",                      (DL_FUNC) &GERGM_get_triad_weights,                       5},
-    {"GERGM_h_statistics",                           (DL_FUNC) &GERGM_h_statistics,                           12},
-    {"GERGM_Individual_Edge_Conditional_Prediction", (DL_FUNC) &GERGM_Individual_Edge_Conditional_Prediction, 30},
-    {"GERGM_Metropolis_Hastings_Sampler",            (DL_FUNC) &GERGM_Metropolis_Hastings_Sampler,            16},
-    {"GERGM_weighted_mple_objective",                (DL_FUNC) &GERGM_weighted_mple_objective,                10},
+    {"GERGM_Distribution_Metropolis_Hastings_Sampler", (DL_FUNC) &GERGM_Distribution_Metropolis_Hastings_Sampler, 27},
+    {"GERGM_Extended_Metropolis_Hastings_Sampler",     (DL_FUNC) &GERGM_Extended_Metropolis_Hastings_Sampler,     29},
+    {"GERGM_extended_weighted_mple_objective",         (DL_FUNC) &GERGM_extended_weighted_mple_objective,         16},
+    {"GERGM_frobenius_norm",                           (DL_FUNC) &GERGM_frobenius_norm,                            2},
+    {"GERGM_get_indiviual_triad_values",               (DL_FUNC) &GERGM_get_indiviual_triad_values,                4},
+    {"GERGM_get_triad_weights",                        (DL_FUNC) &GERGM_get_triad_weights,                         5},
+    {"GERGM_h_statistics",                             (DL_FUNC) &GERGM_h_statistics,                             12},
+    {"GERGM_Individual_Edge_Conditional_Prediction",   (DL_FUNC) &GERGM_Individual_Edge_Conditional_Prediction,   30},
+    {"GERGM_Metropolis_Hastings_Sampler",              (DL_FUNC) &GERGM_Metropolis_Hastings_Sampler,              16},
+    {"GERGM_weighted_mple_objective",                  (DL_FUNC) &GERGM_weighted_mple_objective,                  10},
     {NULL, NULL, 0}
 };
 
