e5f12f0778f256f3fdc6888191cdf4a613e39622
Matt Denny <matthewjdenny@gmail.com>
2017-03-23 14:55:16 -0400

    new function to auto add diagonal term

--- R/Add_Intercept_Term.R
+++ R/Add_Intercept_Term.R
@@ -11,3 +11,18 @@ add_intercept_term <- function(formula) {
   formula <- as.formula(parsed)
   return(formula)
 }
+
+
+add_diagonal_term <- function(formula) {
+  formula <- as.formula(formula)
+  parsed <- deparse(formula)
+  if (length(parsed) > 1) {
+    parsed <- paste0(parsed, collapse = " ")
+  }
+  if (grepl("diagonal",parsed)) {
+    stop("The diagonal term is specified automatically, please remove the 'diagonal' term from your formula.")
+  }
+  parsed <- paste(parsed," + diagonal",sep = "")
+  formula <- as.formula(parsed)
+  return(formula)
+}
