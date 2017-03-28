a8acdffd60e07a3da111d7d55d086a884822d1cb
Matt Denny <matthewjdenny@gmail.com>
2017-03-27 23:52:57 -0400

    now runs one of the tests, deleted second test

--- tests/testthat/test_diagonal_and_distribtuion_estimator.R
+++ tests/testthat/test_diagonal_and_distribtuion_estimator.R
@@ -1,9 +1,5 @@
 test_that("Diagonal model with no covariates runs", {
   skip_on_cran()
-  skip("For time")
-  ########################### 1. No Covariates #############################
-  # Preparing an unbounded network without covariates for gergm estimation #
-  #skip("Skipping test as it can only be run in the global environment.")
 
   set.seed(12345)
   net <- matrix(runif(100),10,10)
@@ -31,49 +27,11 @@ test_that("Diagonal model with no covariates runs", {
 
 })
 
-test_that("Diagonal Model with covariates runs", {
-  skip_on_cran()
-  skip("For time")
-  set.seed(12345)
-  net <- matrix(runif(100,0,1),10,10)
-  colnames(net) <- rownames(net) <- letters[1:10]
-  node_level_covariates <- data.frame(Age = c(25,30,34,27,36,39,27,28,35,40),
-                                      Height = c(70,70,67,58,65,67,64,74,76,80),
-                                      Type = c("A","B","B","A","A","A","B","B","C","C"))
-  rownames(node_level_covariates) <- letters[1:10]
-  network_covariate <- net + matrix(rnorm(100,0,.5),10,10)
-  formula <- net ~ edges + mutual + ttriads + sender("Age") +
-    netcov("network_covariate") + nodemix("Type",base = "A")
-
-  test <- gergm(formula,
-                covariate_data = node_level_covariates,
-                network_is_directed = TRUE,
-                use_MPLE_only = FALSE,
-                estimation_method = "Metropolis",
-                number_of_networks_to_simulate = 100000,
-                thin = 1/100,
-                proposal_variance = 0.5,
-                downweight_statistics_together = TRUE,
-                MCMC_burnin = 50000,
-                seed = 456,
-                convergence_tolerance = 0.01,
-                MPLE_gain_factor = 0,
-                force_x_theta_updates = 2)
-
-  check_against <- c(0.824, -0.072, -0.016, -0.026, -0.024, -0.056, -0.055,
-                     -0.035,  0.002, -0.040, -0.050,  3.061, 0.129, -1.931)
-  expect_equal(c(round(as.numeric(test@theta.coef[1,]),3),round(as.numeric(test@lambda.coef[1,]),3)), check_against)
-
-
-})
 
 
 test_that("Row-wise distribution model runs", {
   skip_on_cran()
   skip("For time")
-  ########################### 1. No Covariates #############################
-  # Preparing an unbounded network without covariates for gergm estimation #
-  #skip("Skipping test as it can only be run in the global environment.")
 
   set.seed(12345)
   net <- matrix(runif(100),10,10)
@@ -83,7 +41,7 @@ test_that("Row-wise distribution model runs", {
   colnames(net) <- rownames(net) <- letters[1:10]
 
   # three parameter model
-  formula <- net ~  edges(method = "endogenous") + mutual + ttriads
+  formula <- net ~ mutual + ttriads
 
   test <- gergm(formula,
                 normalization_type = "division",
@@ -97,9 +55,9 @@ test_that("Row-wise distribution model runs", {
                 MCMC_burnin = 100000,
                 seed = 456,
                 distribution_estimator = "rowwise-marginal",
-                convergence_tolerance = 0.3)
+                convergence_tolerance = 0.1)
 
-  check_against <- c(-2.016,  1.626,  0.118, -0.534)
+  check_against <- c(-2.649,  5.944,  8.490)
   expect_equal(round(as.numeric(test@theta.coef[1,]),3), check_against)
 
 
