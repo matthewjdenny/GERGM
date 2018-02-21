4a5c098d906889b634823889fe9888b7b7ce8455
Matt Denny <matthewjdenny@gmail.com>
2018-02-21 18:57:27 -0500

    tests for new features

--- tests/testthat/test_experimental_features.R
+++ tests/testthat/test_experimental_features.R
@@ -161,3 +161,46 @@ test_that("parallel threading works", {
 
 })
 
+
+test_that("sample at a time works", {
+  skip_on_cran()
+  set.seed(12345)
+  net <- matrix(runif(400,0,1),20,20)
+  colnames(net) <- rownames(net) <- letters[1:20]
+  node_level_covariates <- data.frame(Age = c(25,30,34,27,36,39,27,28,35,40,
+                                              27,56,45,35,24,89,56,45,34,64),
+                                      Height = c(70,70,67,58,65,67,64,74,76,80,
+                                                 60,67,68,69,46,56,67,78,67,77),
+                                      Type = c("A","B","B","A","A","A","B","B","C","C",
+                                               "A","A","A","B","B","B","C","C","C","C"))
+  rownames(node_level_covariates) <- letters[1:20]
+  network_covariate <- net + matrix(rnorm(400,0,.5),20,20)
+
+  formula <- net ~ edges + mutual + ttriads + sender("Age") +
+    netcov("network_covariate") + nodematch("Type",base = "A")
+
+  system.time({
+    test <- gergm(formula,
+                  covariate_data = node_level_covariates,
+                  network_is_directed = TRUE,
+                  number_of_networks_to_simulate = 100000,
+                  thin = 1/1000,
+                  proposal_variance = 0.1,
+                  MCMC_burnin = 100000,
+                  seed = 456,
+                  convergence_tolerance = 0.5,
+                  convex_hull_proportion = 0.9,
+                  convex_hull_convergence_proportion = 0.9,
+                  sample_edges_at_a_time = 229
+    )
+  })
+
+
+
+  check <- c(round(as.numeric(test@theta.coef[1,]),3),round(as.numeric(test@lambda.coef[1,]),3))
+  check2 <- c(round(as.numeric(test2@theta.coef[1,]),3),round(as.numeric(test2@lambda.coef[1,]),3))
+  expect_equal(check, check2)
+
+})
+
+
