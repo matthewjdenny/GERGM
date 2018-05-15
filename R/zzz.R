ae55634b243948235de0f29fd6ff6dfdbebfd089
Matt Denny <matthewjdenny@gmail.com>
2018-05-14 23:17:51 -0400

    version bump

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("GERGM: Generalized Exponential Random Graph Models
-Version 0.12.2 created on 2018-01-15.
+Version 0.13.0 created on 2018-05-14.
 copyright (c) 2018, Matthew J. Denny, Penn State University
                     James D. Wilson, University of San Francisco
                     Skyler Cranmer, Ohio State University
